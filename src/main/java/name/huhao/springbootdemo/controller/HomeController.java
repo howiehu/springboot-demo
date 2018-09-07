a69bb31c624dc0a4db2f7209fbfdba03ff5be2f3
hhu <hhu@thoughtworks.com>
2018-09-07 21:39:58 +0800

    use springfox & add swagger support

--- src/main/java/name/huhao/springbootdemo/controller/HomeController.java
+++ src/main/java/name/huhao/springbootdemo/controller/HomeController.java
@@ -1,14 +1,18 @@
 package name.huhao.springbootdemo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/")
+@Api(tags = "Home", description = "Demo Api")
 public class HomeController {
 
     @GetMapping
+    @ApiOperation(value = "Index")
     public String index() {
         return "Hello World!";
     }
--- src/main/java/name/huhao/springbootdemo/controller/UserController.java
+++ src/main/java/name/huhao/springbootdemo/controller/UserController.java
@@ -1,9 +1,12 @@
 package name.huhao.springbootdemo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import name.huhao.springbootdemo.model.User;
 import name.huhao.springbootdemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Api(tags = "Users", description = "Users Api")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -24,6 +28,7 @@ public class UserController {
 
     @GetMapping
     @ResponseStatus(value = HttpStatus.OK)
+    @ApiOperation(value = "Get users")
     public List<User> index() {
         return userRepository.findAll();
     }
