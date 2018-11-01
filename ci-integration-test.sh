2b0932a34491b437998680dff6fe64e751d45ac9
hhu <hhu@thoughtworks.com>
2018-11-01 11:57:20 +0800

    try to fix ci scripts bug

--- ci-integration-test.sh
+++ ci-integration-test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -v "$PWD":/home/gradle/springboot-demo -w /home/gradle/springboot-demo gradle:4.10.2-jdk11 gradle --no-daemon -s test --tests name.huhao.springbootdemo.integration.*
+docker run --rm -v "$PWD":/home/gradle -w /home/gradle gradle:4.10.2-jdk11 gradle --no-daemon -s test --tests name.huhao.springbootdemo.integration.*
